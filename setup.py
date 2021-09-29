389ac22a1198b6e885f5cfb8d37a593946eb392e
Nilesha14 <nilesh.acharekar@gmail.com>
2021-09-29 11:03:20 +0530

    setup done, update readme

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    description="It is Wine Quality Data Analysis",
+    author="Nilesh Suresh Acharekar",
+    packages = find_packages(),
+    license="MIT"
+
+)
